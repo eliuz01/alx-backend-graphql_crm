58e5600533f6762c23aa8d5abebb45e4d4a26fdb
ELIUZ <osongoeliuz14@gmail.com>
2025-08-19 14:27:31 +0300

    fix error: create database

--- alx_backend_graphql/schema.py
+++ alx_backend_graphql/schema.py
@@ -1,6 +1,6 @@
 import graphene
 
 class Query(graphene.ObjectType):
-    hello = graphene.String(default_value="Hello, GraphQL!")
+    hello = graphene.String(default_value="Hello world!")
 
 schema = graphene.Schema(query=Query)
